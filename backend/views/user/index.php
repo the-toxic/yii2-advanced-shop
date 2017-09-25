8ee9250e85f5761c2be4e5d34ebb856334f9fad4
Evgeniy Guskov <guskov.ep@real.su>
2017-09-25 19:17:40 +0400

    Added users CRUD + small fix

---
+++ backend/views/user/index.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\grid\ActionColumn;
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\forms\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <div class="box">
+        <div class="box-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    'id',
+                    'created_at:datetime',
+                    'username',
+                     'email:email',
+                     'status',
+                    ['class' => ActionColumn::class],
+                ],
+            ]); ?>
+        </div>
+    </div>
+
+</div>
