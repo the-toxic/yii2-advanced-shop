8e3f9a86ab98ebfdd5e8ef4ad87b5405d282c41d
Evgeniy Guskov <guskov.ep@real.su>
2017-10-01 00:13:37 +0400

    Added tag + in backend

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -15,11 +15,11 @@
         </div>
 
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
+        <form action="#" class="sidebar-form">
             <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                <input name="q" class="form-control" placeholder="Search..."/>
               <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+                <button name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
               </span>
             </div>
@@ -32,6 +32,7 @@
                 'items' => [
                     ['label' => 'Управление', 'options' => ['class' => 'header']],
                     ['label' => 'Пользователи', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user'],
+                    ['label' => 'Теги', 'icon' => 'file-o', 'url' => ['/shop/tag/index'], 'active' => $this->context->id == 'shop/tag'],
                 ],
             ]
         ) ?>
---
+++ shop/repositories/Shop/TagRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace shop\repositories\Shop;
+
+use shop\entities\Shop\Tag;
+
+class TagRepository
+{
+    public function get($id): Tag
+    {
+        if (!$tag = Tag::findOne($id)) {
+            throw new \DomainException('Tag is not found.');
+        }
+        return $tag;
+    }
+    public function save(Tag $tag): void
+    {
+        if (!$tag->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+    public function remove(Tag $tag): void
+    {
+        if (!$tag->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
+}
