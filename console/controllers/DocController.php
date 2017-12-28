89c4ef707e3c6d888c064bc28ba174875de3dd59
Evgeniy Guskov <guskov.ep@real.su>
2017-12-28 20:03:26 +0400

    Added API Swagger UI

---
+++ console/controllers/DocController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+
+class DocController extends Controller
+{
+    public function actionBuild(): void
+    {
+        $swagger = Yii::getAlias('@vendor/bin/swagger');
+        $source = Yii::getAlias('@api/controllers');
+        $target = Yii::getAlias('@api/web/docs/swagger.json');
+
+        passthru('"' . PHP_BINARY . '"' . " \"{$swagger}\" \"{$source}\" --output \"{$target}\"");
+
+        // php vendor/bin/swagger api/controllers -o api/web/docs/swagger.json
+    }
+}
